d6924a1f27ebbb7bd7613993c0e8ecf6a7ab08b4
PhucHien21 <hiennguyen210901@gmail.com>
2023-11-02 21:47:29 +0200

    first commit

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleGood} text={'good'}/>
+      <Button onClick={handleNeutral} text={'neutral'}/>
+      <Button onClick={handleBad} text={'bad'}/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+const Button = ({onClick, text}) => {
+  return (
+    <button onClick={onClick}>{text}</button>
+  )
+}
+
+const Statistics = ({good, neutral, bad}) => {
+  const total = good + neutral + bad
+
+  if (total === 0) {
+    return (
+      <div>
+        <h2>statistics</h2>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>statistics</h2>
+      <table>
+        <tbody>
+          <StatisticsLine text={'good'} value={good}/>
+          <StatisticsLine text={'neutral'} value={neutral}/>
+          <StatisticsLine text={'bad'} value={bad}/>
+          <StatisticsLine text={'average'} value={good - bad}/>
+          <StatisticsLine text={'positive'} value={((good/total)*100).toFixed(1) + '%'}/>
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const StatisticsLine = ({text, value}) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+export default App
